3b1f9974025ef182155603fa400337e995b70a0c
linux_china <libing.chen@gmail.com>
2022-11-05 19:16:49 +0800

    chore: add icons for run, test and testFailed

--- src/main/kotlin/org/ziglang/jb/ZigIcons.kt
+++ src/main/kotlin/org/ziglang/jb/ZigIcons.kt
@@ -1,5 +1,6 @@
 package org.ziglang.jb
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
 
 object ZigIcons {
@@ -9,4 +10,8 @@ object ZigIcons {
     val icon = IconLoader.getIcon("/icons/zig_icon.png", this::class.java)
     val sdk = IconLoader.getIcon("/icons/zig_sdk.png", this::class.java)
     val variable = IconLoader.getIcon("/icons/zig_variable.png", this::class.java)
+    var run = IconLoader.getIcon("/runConfigurations/testState/run.svg", AllIcons::class.java)
+    var test = IconLoader.getIcon("/icons/zig_test_mark.svg", this::class.java)
+    val testFailed = IconLoader.getIcon("/runConfigurations/testState/red2.svg", AllIcons::class.java)
+    //val testSucceed = IconLoader.getIcon("/runConfigurations/testState/green2.svg", AllIcons::class.java)
 }
